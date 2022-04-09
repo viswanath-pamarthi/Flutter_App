45a3587ec7a41c62e11e070f666cb1b7059f5d1d
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-04-09 11:08:49 -0400

    - added a scaffold widget to Material App with a App Bar and a network image as body with center widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,19 @@ void main() {
     MaterialApp(
       //Material App widget
       //Material design is a design style or concept created by Google adopted by many companies for Mobile Apps and websites http://material.io
-      home: Center(
-        child: Text(
-            'Hello world'), //Add a "," at after each closing ) of every widget so that the file is automatically formatted with dart format
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('My Flutter App'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+          ),
+        ),
       ),
-    ),
+    ), //Add a "," at after each closing ) of every widget so that the file is automatically formatted with dart format
   );
 }
